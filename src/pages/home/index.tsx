0c6a8cc15568bd8d33aaceef368e98afef98ca1f
Tarcio Diniz <tarciodiniz0@gmail.com>
2024-05-05 20:12:33 -0300

    feat: page home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,87 @@
+import styles from "./home.module.scss"
+import {Button} from "@mui/material";
+import {useNavigate} from "react-router-dom";
+
 const Home = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/form");
+  }
+
   return (
-    <div>Página Home</div>
+    <div className={styles.content}>
+      <div className={styles.stars}>
+        <div className={styles.star}></div>
+        <div className={styles.star}></div>
+        <div className={styles.star}></div>
+        <div className={styles.star}></div>
+        <div className={styles.star}></div>
+        <div className={styles.star}></div>
+        <div className={styles.star}></div>
+        <div className={styles.star}></div>
+        <div className={styles.star}></div>
+        <div className={styles.star}></div>
+        <div className={styles.star}></div>
+        <div className={styles.star}></div>
+        <div className={styles.star}></div>
+        <div className={styles.star}></div>
+        <div className={styles.star}></div>
+        <div className={styles.star}></div>
+        <div className={styles.star}></div>
+        <div className={styles.star}></div>
+        <div className={styles.star}></div>
+        <div className={styles.star}></div>
+        <div className={styles.star}></div>
+        <div className={styles.star}></div>
+        <div className={styles.star}></div>
+        <div className={styles.star}></div>
+        <div className={styles.star}></div>
+        <div className={styles.star}></div>
+        <div className={styles.star}></div>
+        <div className={styles.star}></div>
+        <div className={styles.star}></div>
+        <div className={styles.star}></div>
+        <div className={styles.star}></div>
+        <div className={styles.star}></div>
+        <div className={styles.star}></div>
+        <div className={styles.star}></div>
+        <div className={styles.star}></div>
+        <div className={styles.star}></div>
+        <div className={styles.star}></div>
+        <div className={styles.star}></div>
+        <div className={styles.star}></div>
+        <div className={styles.star}></div>
+        <div className={styles.star}></div>
+        <div className={styles.star}></div>
+        <div className={styles.star}></div>
+        <div className={styles.star}></div>
+        <div className={styles.star}></div>
+        <div className={styles.star}></div>
+        <div className={styles.star}></div>
+        <div className={styles.star}></div>
+        <div className={styles.star}></div>
+        <div className={styles.star}></div>
+      </div>
+      <div className={styles.container}>
+        <div className={styles.container_interno}>
+          <h2>BEM-VINDO AO PROJETO 6</h2>
+
+          <p>UNIVERSIDADE ESTADUAL DA PARAÍBA</p>
+          <p>DISCIPLINA: Tecs. de Desenvolvimento de Interface Gráfica</p>
+          <p>PROFESSOR: Ramon Bezerra da Nóbrega</p>
+          <p>ALUNO: Tarcio Elyakin Agra Diniz</p>
+
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleClick}
+          >
+            Faça sua pesquisa
+          </Button>
+        </div>
+      </div>
+    </div>
   );
 }
 
