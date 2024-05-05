fbed0c3eebfb98f8056ce62e0a4e4e3af0a7966c
Tarcio Diniz <tarciodiniz0@gmail.com>
2024-05-05 16:13:32 -0300

    fix: bug possível erro de rotas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import Home from "../pages/home";
 import FormPage from "../pages/form";
 import Faq from "../pages/faq";
@@ -6,14 +6,12 @@ import Page404 from "../pages/page404";
 
 const Router = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/*" element={<Page404/>}/>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/form" element={<FormPage/>}/>
-        <Route path="/faq" element={<Faq/>}/>
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/*" element={<Page404/>}/>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/form" element={<FormPage/>}/>
+      <Route path="/faq" element={<Faq/>}/>
+    </Routes>
   );
 }
 
