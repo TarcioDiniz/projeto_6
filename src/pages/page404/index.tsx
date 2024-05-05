ee7404b85968fc28b6771e25d648840c14169463
Tarcio Diniz <tarciodiniz0@gmail.com>
2024-05-05 16:46:29 -0300

    feat: tela 404

--- src/pages/page404/index.tsx
+++ src/pages/page404/index.tsx
@@ -1,6 +1,18 @@
+import styles from "./page404.module.scss"
+import {Player} from "@lottiefiles/react-lottie-player";
+import animation from "../../assets/animation_page_404.json"
+
 const Page404 = () => {
   return (
-    <div>Pagina 404</div>
+    <div className={styles.content}>
+      <Player
+        autoplay
+        loop
+        src={animation}
+        style={{ height: '50%' }}
+      >
+      </Player>
+    </div>
   );
 }
 
