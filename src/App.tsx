9a04ba774691bfd912381af700a413dde4e47ef2
Tarcio Diniz <tarciodiniz0@gmail.com>
2024-05-05 16:23:42 -0300

    feat: navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Router from "./routes";
+import Navbar from "./layout/navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <Router/>
     </div>
   );
---
+++ src/layout/navbar/index.tsx
@@ -0,0 +1,58 @@
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import { MagicTabSelect } from "react-magic-motion";
+import styles from "./navbar.module.scss"
+
+const tabData = [
+  { title: 'INICIO', route: '/' },
+  { title: 'FORMULÁRIO', route: '/form' },
+  { title: 'DUVIDAS', route: '/faq' }
+];
+
+const Navbar = () => {
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const navigate = useNavigate();
+
+  const handleTabClick = (index: number, route: string) => {
+    setSelectedIndex(index);
+    navigate(route);
+  };
+
+  return (
+    <div className={styles.header}>
+      <h2>PROJETO 6</h2>
+      <div>
+        {tabData.map((tab, i) => (
+          <button
+            type="button"
+            key={`tab-${i}`}
+            onClick={() => handleTabClick(i, tab.route)}
+            className={styles.buttonTab}
+          >
+            {tab.title}
+
+            {selectedIndex === i && (
+              <div style={{position: "relative", transform: "translateY(3px)"}}>
+                <MagicTabSelect
+                  id="underline"
+                  transition={{type: "spring", bounce: 0.3}}
+                >
+                  <div
+                    style={{
+                      width: "100%",
+                      height: "0.15rem",
+                      backgroundColor: "white",
+                      position: "absolute",
+                    }}
+                  />
+                </MagicTabSelect>
+              </div>
+            )}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
